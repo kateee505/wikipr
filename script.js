2781af00f654a8a1ece3873952642bf983230377
kateee505 <150558093+kateee505@users.noreply.github.com>
2026-01-18 23:31:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 console.log("JS pieslēgts un darbojas");
 
+
+document.getElementById("poga").addEventListener("click", function () {
+    document.getElementById("teksts").style.color = "blue";
+    document.getElementById("teksts").textContent = "Poga tika nospiesta!";
+});
+
+
+document.getElementById("vards").addEventListener("input", function () {
+    document.getElementById("teksts").textContent =
+        "Sveiki, " + this.value + "!";
+});
+
 document.getElementById("poga").addEventListener("click", function () {
     alert("JavaScript darbojas!");
 });
